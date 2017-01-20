f36c5eea23321861ba4b72e17f6bf7884bd1145b
Anton Freudenthaler <anton.p.v.freudenthaler@gmail.com>
2017-01-20 12:34:22 +0100

    Initial Commit

---
+++ DigDug/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DigDug
+//
+//  Created by Anton Freudenthaler on 20/01/17.
+//  Copyright © 2017 Anton Freudenthaler. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
